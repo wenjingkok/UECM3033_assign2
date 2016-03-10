cc09242a63ef05cabb98973d30477956bcde5a7c
wenjingkok <wjingkok@1utar.my>
2016-03-11 04:39:28 +0800

    Revert "update"
    This reverts commit 54a3280c8f02a7476a262590c9ead68f2216c3b1.

--- task1.py
+++ task1.py
@@ -1,77 +1,19 @@
 import numpy as np
-import scipy
-import scipy.linalg as linalg
+#Your optional code here
+#You can import some modules or create additional functions
 
 def lu(A, b):
     sol = []
-    # Edit here to implement your code    
-    L , U = linalg.lu(A, True)
-    y = linalg.solve(L,b)
-    x = linalg.solve(U, y)
-    sol.append(x)
+    # Edit here to implement your code
     return list(sol)
 
 def sor(A, b):
     sol = []
     # Edit here to implement your code
-    maxiter = 15
-    n = len(A)
-    #A = D - L - U
-    D = np.zeros((n,n))
-    L = np.zeros((n,n))
-    U = np.zeros((n,n))
-    for i in range(0, n):
-        D[i][i] = A[i][i]
-    for i in range(0, n):
-        for j in range(0, i):
-            L[i][j] = -A[i][j]
-    for i in range(0, n):
-        for j in range(i+1, n):
-            U[i][j] = -A[i][j]
-    
-    #K = inv(D)(L+U)
-    K = np.zeros((n,n))
-    for i in range(0, n):
-        K[i][i] = 1/D[i][i]
-    K = np.dot(K,(L+U))
-    lamda = max(abs(linalg.eigvals(K)))
-    lamda = np.square(lamda)
-    omega = 2 * (1 - np.sqrt(1-lamda)) / lamda
-    
-    Q = np.zeros((n,n))
-    Q = 1/omega * (D - omega * L)
-    x = np.zeros(n)
-    for iteration in range(0, maxiter):
-        x = np.dot(np.dot(np.linalg.inv(Q), (Q-A)) , x) + np.dot(np.linalg.inv(Q) , b)
-        sol.append(x)
-        
     return list(sol)
 
-def check(A):
-    try:
-        temp = 2 * np.diag(A) > np.sum(np.abs(A),1)
-        result = temp.all()
-        
-        if (result):
-            #A is Strictly Diagonally dominant Matrix
-            return True
-            
-        #Check positive Definite Matrix
-        np.linalg.cholesky(A)
-        
-        #positive diaganal element
-        temp = np.diag(A) > 0
-        result = temp.all()
-        if (~result):
-            #some diagonal element is negative
-            return True
-    except np.linalg.linalg.LinAlgError:
-        #Solve by LU
-        return True
-    return False
-
 def solve(A, b):
-    condition = check(A) # State and implement your condition here
+    condition = True # State and implement your condition here
     if condition:
         print('Solve by lu(A,b)')
         return lu(A,b)
